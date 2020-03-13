1bcf4ed5b5a93e72b0c7ec2f5c66bd178fa9e250
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2020-03-13 12:18:27 -0300

    Add files via upload

---
+++ 3TEC - 2020/OurSpace/MenuTopo.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg bg-info">
+    <div class="collapse navbar-collapse">
+        <div class="nav navbar-brand col-lg-2">
+            <i class="fa fa-globe fa-lg text-white"></i>
+            <font class="TituloLogo">OurSpace</font>
+        </div>
+
+        <div class="col-lg-3">
+            <form action="" method="" id="">
+                <div class="form-group">
+                    <div class="input-group"style="padding-top:15px;">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fa fa-search fa-lg"></i>
+                            </span>
+                        </div>
+                        <input type="text" name="HTML_usuario" placeholder="Pesquisar fotos" class="form-control">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-outline-info bg-white">Pesquisar</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-lg-1"></div>
+        <div class="navbar nav col-lg-4">
+            <a href="" class="LinkMenu">Alterar Dados</a>
+            <a href="" class="LinkMenu">Amigos</a>
+            <a href="" class="LinkMenu">Publicar</a>
+            <a href="" class="LinkMenu">Explorar</a>
+        </div>
+        <div class="col-lg-2">
+            <i class="fa fa-user fa-2x text-white float-right"></i> 
+        </div>
+
+    </div>   
+</nav>
---
+++ 3TEC - 2020/OurSpace/index.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+
+        <meta charset="UTF-8">
+        <title>::OurSpace - You will never be alone again::</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/estiloVintage.css">
+
+    </head>
+    <body>
+<div class="container d-flex" style="justify-content:center;padding-top: 150px;">
+            <div class="col-lg-7 border">
+                
+                    <form action="Home.php" method="post">
+                        <p>Preencha os dados abaixo para acessar o sistema</p>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-user fa-lg"></i>
+                                    </span>
+                                </div>
+                            <input type="text" name="HTML_usuario" placeholder="Nome de Usuário" class="form-control">
+                        
+                            </div>
+                        </div>
+
+                        
+                        <div class="form-group"> 
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-key fa-lg"></i>
+                                    </span>
+                                </div>
+                            <input type="password" name="HTML_senha" placeholder="Senha" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            
+                            <button class="btn btn-info">
+                                <i class="fa fa-users fa-lg"></i> Quero fazer parte!                                
+                            </button>
+                            
+                            <button type="submit" class="btn btn-info float-right">
+                                <i class="fa fa-globe fa-lg"></i> Entrar no Space      
+                            </button>            
+                        </div>
+
+                    </form>
+            </div>
+                </div>
+            </center>
+     
+    </body>
+</html>
