f029b06f405e5d991d9e3053b90c2ccf3276c495
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2021-03-12 11:14:08 -0300

    Add files via upload

---
+++ 2TEC - 2021/USWorld/index.php
@@ -0,0 +1,118 @@
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <!-- Construção do Conteúdo -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    </head>
+
+    <body bgcolor="#CD0000">
+        <!-- Configurações da Página -->
+        
+        <?php
+            include "MenuTopo.php";
+        ?>
+        
+        <div class="container">
+
+            <div class="row"> <!-- 1° linha -->
+                <div class="col-lg-12" style="min-height:140px;">
+                    <img src="img/bannerUS2.jpg" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="row"> <!-- 2° linha -->
+                <div class="col-lg-12" style="min-height:20px;">
+                </div>
+            </div>
+
+            <div class="row"> <!-- 3° linha -->
+
+                <div class="col-lg-4" style="min-height:250px;">
+                    <div class="card" style="margin-top:15px;">
+                        <img src="img/news.jpg" class="img-card">
+                        <div class="card-body">
+                            <h4>NEWS</h4>
+                            Acompanhe as últimas notícias em destaque nos EUA.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4" style="min-height:250px;">
+                    <div class="card" style="margin-top:15px;">
+                        <img src="img/geo.jpg" class="img-card">
+                        <div class="card-body">
+                            <h4>Our Country</h4>
+                            Conheça a divisão política, estados e geografia norte-americana.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4" style="min-height:250px;">
+                    <div class="card" style="margin-top:15px;">
+                        <img src="img/lands.jpg" class="img-card">
+                        <div class="card-body">
+                            <h4>Landmarks</h4>
+                            Desfrute dos locais mais icônicos do mundo dentro dos EUA.
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row"> <!-- 4° linha -->
+                <div class="col-lg-4" style="min-height:250px;">
+                    <div class="card" style="margin-top:15px;">
+                        <img src="img/politics.jpg" class="img-card">
+                        <div class="card-body">
+                            <h4>Politics</h4>
+                            Conheça as leis e como funcionam as eleições norte-americanas.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4" style="min-height:250px;">
+                    <div class="card" style="margin-top:15px;">
+                        <img src="img/cities.jpg" class="img-card">
+                        <div class="card-body">
+                            <h4>Cities</h4>
+                            Informações sobre as principais cidades do país.
+                        </div>
+                    </div>
+                </div>
+            
+
+            <div class="col-lg-4" style="min-height:250px;"><div class="card" style="margin-top:15px;">
+                    <img src="img/shopping.jpg" class="img-card">
+                    <div class="card-body">
+                        <h4>Shopping</h4>
+                        Melhores locais de compra de acordo com as taxas de cada estado.
+                    </div>
+                </div>
+            </div>
+</div>
+        
+
+    <div class="row"> <!-- 5° linha -->
+        <div class="col-lg-6" style="min-height:250px;">
+        </div>
+        <div class="col-lg-6" style="min-height:250px;">
+        </div>
+    </div>
+
+    <div class="row"> <!-- 6° linha -->
+        <div class="col-lg-12" style="min-height:50px;">
+        </div>
+    </div>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</body>
+
+
+</html>
